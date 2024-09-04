5ee1d3881436a57afc8c8340db8c9d048b893f6a
anamiikkkaaaa <anamikarajput18564@gmail.com>
2024-09-04 01:05:09 +0000

    variables and datatypes changes saved

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountID = 12534; /* can't change*/
+let accountEmail = "anamika@gmail.com"; /* cares about scope */
+var accountPassword = 1234; /*don't use this */
+accountCity = "jaipur";
+
+console.log([accountID, accountEmail, accountPassword, accountCity]);console.log([accountID, accountEmail, accountPassword, accountCity]);
+console.table([accountID, accountEmail, accountPassword, accountCity]);console.log([accountID, accountEmail, accountPassword, accountCity]);
+let accountState; /*undefined*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" /* all js code will be treated as newer version */
+/* alert(3+3) use in browser only yaha par error aa jayega , we're using node.js not browser*/
+/*datatypes:
+number
+bigint
+string
+boolean
+null : empty
+undefined
+symbol : unique
+object
+*/
+let age = 5;
+typeof "hi";
+typeof age;
+typeof null;
+typeof undefined;
