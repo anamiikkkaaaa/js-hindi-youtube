83aec002d241205cc742c576879af601915bc5a6
anamiikkkaaaa <anamikarajput18564@gmail.com>
2024-09-08 15:34:47 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,13 @@
+const name = "hitesh";
+const repoCount = 50;
+
+console.log(name +repoCount = "Value") ; //outdated syntax
+
+//string interpolation, we make placeholders, using back ticks
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hiteshhc');
+
+console.log(gameName[0]);
+console.log(gameName.__proto__.);
--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -27,3 +27,23 @@ const myFunction = function(){
     console.log("Hello world");
 }
 //typeof null is object, and typeof undefined is undefined.
+
+
+//stack(primitive datatype): copy of the variable , heap(non-primitive): reference to original value
+
+let myYoutubename ="hiteshchoudharydotcom";
+
+let anothername = myYoutubename;
+anothername = "chaiaurcode";
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne ={
+    email:"user@gmail.com",
+    upi:"user@ybl",
+};
+let userTwo = userOne;
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
