3492d9138a99bf9c63a0879013db84ba0c399137
anamiikkkaaaa <anamikarajput18564@gmail.com>
2024-09-11 10:25:35 +0000

    function done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,3 +36,39 @@ function loginUserMessage(username){ // or give username a default value usernam
 
 console.log(loginUserMessage("hitesh"));
 console.log(loginUserMessage());
+
+
+/*
+function calculateCartPrice(...num1){   // spread operation or rest operator 
+    return num1
+}
+console.log(calculateCartPrice(200, 400, 500));
+*/
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject(user);
+
+handleObject({
+    username: "sam",
+    price: 399,
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
