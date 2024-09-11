3f3eb9bf2b3d8e70fc25cf9fc1f24008832be7f6
anamiikkkaaaa <anamikarajput18564@gmail.com>
2024-09-11 10:11:44 +0000

    functions part 1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,38 @@
+function sayMyName(){
+    console.log("H");
+    console.log("I")
+    console.log("T");
+    console.log("E");
+    console.log("S")
+    console.log("H");
+}
+
+sayMyName();
+
+/* function addTwoNumbers(num1,num2){
+    console.log(num1 + num2 );
+}
+addTwoNumbers(2,3);
+addTwoNumbers(3, "a");
+
+const result = addTwoNumbers(3,5);
+console.log("result: ", result); //result will be undefined because function is not returning anything is it just printing
+*/
+function addTwoNumbers(num1,num2){
+    let result = num1 + num2;
+    return result;
+    console.log("hitesh") //this is unreachable code as it is after return 
+}
+const result = addTwoNumbers(3,5);
+console.log("result: ", result);
+
+function loginUserMessage(username){ // or give username a default value username = "sam"
+    if(username === undefined){  // or (!username)
+        console.log("please enter a username");
+        return;
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("hitesh"));
+console.log(loginUserMessage());
